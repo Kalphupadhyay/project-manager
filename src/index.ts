b2bd93d4ac4b519be5e38ad8244254d635a5e877
Kalph upadhyay <kalpupadhyay@gmail.com>
2025-09-27 14:13:53 +0530

    feat: add express server setup and dotenv configuration
    - Added express as a dependency and its types for TypeScript.
    - Created a new app.ts file to define a simple Express server with a root route.
    - Updated index.ts to configure dotenv and start the server on a specified port.
    - Changed TypeScript module setting from commonjs to ES2020 for compatibility.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("working as expected");
+
+import dotenv from 'dotenv';
+import app from './app.js';
+
+dotenv.config();
+
+
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
